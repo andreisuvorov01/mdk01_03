ce6698b9077fc82a2d6fbaa9471d9ed9e9d30311
dron2309 <andrei03jump@gmail.com>
2022-10-04 18:54:49 +0300

    practice1

---
+++ practice1/src/main/kotlin/Main.kt
@@ -0,0 +1,74 @@
+import kotlin.math.PI
+
+fun main(args: Array<String>) {
+    //первое задание Суворов андрей ИС-32
+    val exercises:Int = 13
+    var exercisesSolved: Int = 0
+    var KolvoZadach:Int = 1
+    //второе задание
+    val Myage:Int = 18
+    KolvoZadach++
+    //третье задание
+    var MyAgeStart:Double = 18.0
+    val human = 30
+    MyAgeStart = (MyAgeStart + human)/2
+    KolvoZadach++
+    //четвёртое задание
+    val testNumber:Double = 27.0
+    val evenOdd:Double = testNumber % 2
+    println(evenOdd)
+    KolvoZadach++
+    //пятое задание
+    var answer:Int = 0
+    answer++
+    answer += 10 * 10
+    answer shr 3
+    println(answer)
+    KolvoZadach++
+    //ответ будет 101
+    //шестое задание
+    var age:Int
+    age = 16
+    println(age)
+    age = 30
+    println(age)
+    KolvoZadach++
+    //седьмое задание
+    val a:Int = 46
+    val b:Int = 10
+    val  answer1: Int = (a*100) + (b/10)
+    val  answer2: Int = (a*100) + (b*100)
+    val  answer3: Int = (a*100) + b
+    println(answer1)
+    println(answer2)
+    println(answer3)
+    KolvoZadach++
+    //восьмое задание
+    ((5 * 3) - ((4 / 2) * 2))
+    KolvoZadach++
+    //девятое задание
+    val a1:Double = 11.0
+    val b1:Double = 22.0
+    val average:Double = (a1 + b1)/2
+    println(average)
+    KolvoZadach++
+    //десятое задание
+    val fahrenheit: Double = 100.0
+    val celsii:Double = fahrenheit *1.8 +32
+    println(celsii)
+    KolvoZadach++
+    //212 градусов цельсия
+    //11 задание
+    val position: Int = 63
+    val row:Int = 7
+    val column:Int = 7
+    KolvoZadach++
+    //12 задание
+    val degrees:Double = 90.0
+    var radians:Double = degrees/ PI
+    println(radians)
+    KolvoZadach++
+    //13 задание
+    val x1: Double; val x2:Double; val y1:Double; val y2:Double
+    val distance:Double = (x1+y1) - (x2+y1)
+}
