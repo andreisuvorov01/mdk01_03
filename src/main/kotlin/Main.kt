e92249cda76492ce745e9035a4ea434dbf28acfd
dron2309 <andrei03jump@gmail.com>
2022-10-10 22:47:28 +0300

    практика номер два

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+fun main() {
+  //задача 1
+    val age1 = 42
+    val age2 = 21
+    // тип обоих перменных Int
+
+  //задача 2
+  val avg1 = (age1 + age2)/2
+    // Тип Int. всё правильно
+  //задача 3
+
+  //задача 4
+    val firstName: String = "Андрей"
+
+    val lastname : String = "Суворов"
+  //задача 5
+    val fullName : String = firstName + " " + lastname
+
+  //задача 6
+      println("Привет меня зовут $firstName $lastname")
+
+  //задача 7
+    val Triple : Triple<Int, Int, Int> = Triple(10, 10, 2022)
+
+  //задача 8
+    val month : Int = Triple.first
+    val day : Int = Triple.second
+    val year : Int = Triple.third
+
+  //задача 9
+    
+}
