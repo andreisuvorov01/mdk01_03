0a88deb3510f4bbb975c054336ab61829a2e4863
dron2309 <andrei03jump@gmail.com>
2022-11-02 21:09:07 +0300

    practice13

--- app/src/main/java/com/example/edittext/MainActivity.kt
+++ app/src/main/java/com/example/edittext/MainActivity.kt
@@ -13,30 +13,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun convertButton(View : View) {
-        val editText1 : EditText = findViewById(R.id.editTextTextPersonName2)
-        val string1 :String =  editText1.text.toString()
-        val doll :Int? = string1.toIntOrNull()
-        val editText2 : EditText = findViewById(R.id.editTextTextPersonName3)
-        val string2 : String = editText2.text.toString()
-        val rubl : Int? = string2.toIntOrNull()
-        val textView1 : TextView = findViewById(R.id.textView4)
-        if(doll != null && rubl != null) {
-            val formula : Int = rubl * doll
-            val result : String = formula.toString()
-            textView1.text = result
-        }
-        else {
-            val toast =Toast.makeText(applicationContext,"курс обмена не указан", Toast.LENGTH_LONG)
+    fun convertButton(View: View) {
+        val editText1: EditText = findViewById(R.id.editTextTextPersonName2)
+        val string1: String = editText1.text.toString()
+        val doll: Int? = string1.toIntOrNull()
+        val editText2: EditText = findViewById(R.id.editTextTextPersonName3)
+        val string2: String = editText2.text.toString()
+        val rubl: Int? = string2.toIntOrNull()
+        val textView1: TextView = findViewById(R.id.textView4)
+        if (doll == null && rubl == null) {
+            val toast =
+                Toast.makeText(applicationContext, "оба из полей пустые", Toast.LENGTH_LONG)
             toast.show()
-        }
-        if(rubl != null){
+        } else if (rubl == null) {
             val toast = Toast.makeText(applicationContext, "Сумма не указана", Toast.LENGTH_LONG)
             toast.show()
+        } else {
+            if (doll == null ) {
+                val toast =
+                    Toast.makeText(applicationContext, "курс обмена не указан", Toast.LENGTH_LONG)
+                toast.show()
+            } else {
+                val formula: Int? = rubl?.div(doll)
+                val result: String = formula.toString()
+                textView1.text = result
+                return
+            }
         }
-        else {
-            val toast = Toast.makeText(applicationContext,"Оба из полей пустые", Toast.LENGTH_LONG)
-            toast.show()
-        }
+
     }
 }
